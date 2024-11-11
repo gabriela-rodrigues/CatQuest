6ba5158bc9c3dbe21595965e67ac6ef1e9b6db11
gabrielarodrigues-ux <gabriela.s.rodrigues07@gmail.com>
2024-11-10 22:34:14 -0300

    animação do player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,26 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rbPlayer;
     [SerializeField] float speed = 5f;
-    [SerializeField] float jumpForce = 15f;
+    [SerializeField] float jumpForce = 3f;
     [SerializeField] bool isJump;
     [SerializeField] bool inFloor = true;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundLayer;
 
+    Animator animPlayer;
+
+
     private void Awake()
     {
+        animPlayer = GetComponent<Animator>();
         rbPlayer = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
         inFloor = Physics2D.Linecast(transform.position, groundCheck.position, groundLayer);
         Debug.DrawLine(transform.position, groundCheck.position, Color.blue);
+
+        animPlayer.SetBool("Jump", !inFloor);
 
         if (Input.GetButtonDown("Jump") && inFloor)
             isJump = true;
@@ -37,6 +43,8 @@ public class PlayerMovement : MonoBehaviour
     {
         float xMove = Input.GetAxis("Horizontal");
         rbPlayer.velocity = new Vector2(xMove * speed, rbPlayer.velocity.y);
+
+        animPlayer.SetFloat("Speed", Mathf.Abs(xMove));
 
         if (xMove > 0 )
         {
